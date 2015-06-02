0124b4473891412dc9a861b882ffb10bcf319287
neethi <shettyneethi5@gmail.com>
2015-06-02 13:20:43 +0530

    Nee's calci change

--- test.php
+++ test.php
@@ -3,11 +3,10 @@ $a = 3;
 $b = 4;
 $c = $a+$b;
 echo $c;
-$d = $c * $a * $b;
+$d = $a-$b;
 echo $d;
-$e = $d + $c;
+$e = $a*$b;
 echo $e;
-$nee = $a - $b * $e;
-echo $nee;
-$p;
+$f = $a % $b;
+echo $f;
 ?>
